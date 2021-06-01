d501aa57acdcaff0549b4bca4c71aba8582ec7ce
franklin <franklin.liot@edu.escp.eu>
2021-06-01 22:57:43 +0200

    export possible en csv

--- scrape.py
+++ scrape.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests 
+import pandas as pd
 
+filename = "computerRank10.csv"
 
 html_text = requests.get("https://www.lenstore.fr/c1/lentilles-journalieres").text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all("h3", class_ ="c-product-list__title")
-for job in jobs:
+
+npo_jobs = {}
+jobs_no = 0
+
+for index, job in enumerate(jobs):
     company_name = job.text.replace("</a>", "" )
-    company_name = company_name.text.replace("\n", "")
-    print(f'''
-    Nom du modèle: {company_name}
-    ''')
+    print(company_name.strip())
+    jobs_no += 1
+    npo_jobs[jobs_no] = [company_name.strip()]
+
+npo_jobs_df = pd.DataFrame.from_dict(npo_jobs, orient='index', columns=['Nom'])
+
+npo_jobs_df.to_csv('npo_jobs.csv')
