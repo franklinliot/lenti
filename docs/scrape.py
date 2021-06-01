fca8b537063d2d9da1f3b19d74e48bd6966749ac
franklin <franklin.liot@edu.escp.eu>
2021-06-01 18:40:57 +0200

    scrape des noms fonctionnel, mais léger problème de mise en page

--- docs/scrape.py
+++ docs/scrape.py
@@ -4,7 +4,10 @@ import requests
 
 html_text = requests.get("https://www.lenstore.fr/c1/lentilles-journalieres").text
 soup = BeautifulSoup(html_text, 'lxml')
-models = soup.find("ul", { "id" : "ProductList" })
-
-
-print(models)
+jobs = soup.find_all("h3", class_ ="c-product-list__title")
+for job in jobs:
+    company_name = job.text.replace("</a>", "" )
+    company_name = company_name.text.replace("\n", "")
+    print(f'''
+    Nom du modèle: {company_name}
+    ''')
