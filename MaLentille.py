e21084f2060c1f5a16f2208a8058bc376003be02
franklin <franklin.liot@edu.escp.eu>
2021-06-02 15:46:57 +0200

    4 tableaux csv

--- MaLentille.py
+++ MaLentille.py
@@ -4,17 +4,28 @@ import pandas as pd
 
 html_text = requests.get("https://www.malentille.com/36-journaliere-36").text
 soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all("h3")
+jobs = soup.find_all("li", class_ = "ajax_block_product")
 
 npo_jobs = {}
 jobs_no = 0
 
-for index, job in enumerate(jobs):
-    company_name = job.text.replace("</a>", "" )
-    print(company_name.strip())
-    jobs_no += 1
-    npo_jobs[jobs_no] = [company_name.strip()]
+for job in jobs:
+    nomProduit = job.find("h3")
+    nomProduitAdjusted = nomProduit.text.replace("</a>", "")
 
-npo_jobs_df = pd.DataFrame.from_dict(npo_jobs, orient='index', columns=['Nom'])
+    prixProduit = job.find("font", class_ = "category_price")
+    prixProduitAdjusted = prixProduit.text.replace("</font>", "")
+
+    lienAchat = job.h3.a['href']
+
+    revendeur = "MaLentille"
+
+    jobs_no +=1
+    npo_jobs[jobs_no]= [nomProduitAdjusted, prixProduitAdjusted, lienAchat, revendeur]
+    
+    print('nomProduitAdjusted', nomProduitAdjusted, '\nprixProduitAdjusted', prixProduitAdjusted, '\nlienAchat', lienAchat, '\nrevendeur', revendeur)
+
+
+npo_jobs_df = pd.DataFrame.from_dict(npo_jobs, orient='index', columns=['nomProduitAdjusted', 'prixProduitAdjusted', 'lienAchat', 'revendeur'])
 
 npo_jobs_df.to_csv('csv/MalentilleDaily.csv')
