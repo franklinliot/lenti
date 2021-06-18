3683a9ad8d741754d4e9be877ff1f7f5abdfd3ad
franklin <franklin.liot@edu.escp.eu>
2021-06-18 15:23:12 +0200

    deleted unnecessary files

--- .history/ultime_20210617124732.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import glob
-
-df = pd.concat(map(pd.read_csv, ['data/LenStore/LenStore.csv', 'data/LenStore/LentillesMoinsCheres.csv']))
-
-
-df.to_csv(r'/home/franklin/coding/lenti/data/ultime/Ultime.csv')
--- .history/ultime_20210617144348.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.concat(map(pd.read_csv, ['data/LenStore/LenStore.csv', 'data/LentMCheres/LentillesMoinsCheres.csv', 'data/MaLentille/MaLentille.csv', 'data/VisionDirect/VisionDirect.csv']))
-
-
-df.to_csv(r'/home/franklin/coding/lenti/data/ultime/Ultime.csv')
